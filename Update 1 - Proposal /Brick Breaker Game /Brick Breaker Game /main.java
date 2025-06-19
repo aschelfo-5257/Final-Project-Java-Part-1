0e38a12cb9364dde3250bdc0b072a3458f2ea851
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-19 13:53:43 -0400

    main.java

--- Update 1 - Proposal /Brick Breaker Game /Brick Breaker Game /main.java
+++ Update 1 - Proposal /Brick Breaker Game /Brick Breaker Game /main.java
@@ -2,13 +2,18 @@ package brickBracker;
 
 import javax.swing.JFrame;
 
-public class void main(String[] args) {
-  // 1000 auto-generated method stub
-
-  JFrame obj = new JFrame()
-    obj.setBounds(10, 10, 700, 600);
-    obj.setTitle("Breakout Ball");
-    obj.setResizable(false);
-    obj.setVisible(true);
-    obj.setDefaultCloseOperation(JFrame.EXIT ON CLOSE);
+public class Main {
+    public class void main(String[] args) {
+    // 1000 auto-generated method stub
+      JFrame obj = new JFrame()
+      Gameplay gamePlay = new Gameplay();
+      obj.setBounds(10, 10, 700, 600);
+      obj.setTitle("Breakout Ball");
+      obj.setResizable(false);
+      obj.setVisible(true);
+      obj.setDefaultCloseOperation(JFrame.EXIT ON CLOSE);
+      obj.add(Gameplay);
+  }
 }
+
+
