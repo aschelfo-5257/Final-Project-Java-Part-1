f0a25358e9fa98472e1791a361518377589c7b59
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-18 20:47:46 -0400

    main.java

--- Final Project: Update 1 - Proposal /Brick Breaker Game /main.java
+++ Final Project: Update 1 - Proposal /Brick Breaker Game /main.java
@@ -1,1 +1,14 @@
-public 
+package brickBracker;
+
+import javax.swing.JFrame;
+
+public class void main(String[] args) {
+  // 1000 auto-generated method stub
+
+  JFrame obj = new JFrame()
+    obj.setBounds(10, 10, 700, 600);
+    obj.setTitle("Breakout Ball");
+    obj.setResizable(false);
+    obj.setVisible(true);
+    obj.setDefaultCloseOperation(JFrame.EXIT ON CLOSE);
+}
