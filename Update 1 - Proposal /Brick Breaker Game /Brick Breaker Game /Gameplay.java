0464b37ab52b0b8ba128fa23672a6a6344e40014
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-24 13:53:16 -0400

    Gameplay.java

--- Update 1 - Proposal /Brick Breaker Game /Brick Breaker Game /Gameplay.java
+++ Update 1 - Proposal /Brick Breaker Game /Brick Breaker Game /Gameplay.java
@@ -213,5 +213,16 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 }
 public class Main {
         JFrame obj = new JFrame();
-        Gameplay gamePlay = new Gameplay(); // Make sure Gameplay is in the same package or import it
+        public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
 
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        obj.setVisible(true);
+        Gameplay gamePlay = new Gameplay(); // Make sure Gameplay is in the same package or import it
+}
+        
